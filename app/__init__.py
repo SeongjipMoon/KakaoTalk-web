ce190b784d25b804660f8f684c494f7418c9d6b8
agurimon <agurimon18@gmail.com>
2020-02-07 20:04:33 +0900

    check login-session

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,23 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 from app import routes
+from app.tools import call_token
+from app.constants import *
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    me = None
+    access_token = call_token()
+
+    url = 'https://kapi.kakao.com/v1/api/talk/profile'
+    headers = {
+        'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+        'Authorization': "Bearer " + str(access_token) }
+    response = requests.post(url, headers=headers)
+
+    data = json.loads(response.text)
+    if 'nickName' in data:
+        me = data
+
+    return render_template('index.html', me=me)
