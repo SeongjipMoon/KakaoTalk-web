ed7187eb38a166a850462afd4d9a61e2acbc5a44
agurimon <agurimon18@gmail.com>
2020-02-09 03:33:16 +0900

    chatting modal

--- app/static/script/layout.js
+++ app/static/script/layout.js
@@ -3,5 +3,10 @@ require(['/static/config.js'], function () {
         $(document).on('click', '.link', function(e) {
             $(location).attr('href', '' + '/' + $(e.target).attr('menu'))
         });
+
+        $(document).on('click', '.chatting', function(e) {
+            console.log($(e.target).attr('1-2'))
+            $('#modal-' + '1-2').modal('show');
+        });
     });
 });
