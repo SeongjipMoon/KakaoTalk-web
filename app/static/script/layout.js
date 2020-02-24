fc125bb12ee7213183437ffcda56db0185c88522
agurimon <agurimon18@gmail.com>
2020-02-24 13:03:39 +0900

    modal finish

--- app/static/script/layout.js
+++ app/static/script/layout.js
@@ -1,22 +1,13 @@
 require(['/static/config.js'], function () {
     require(['jquery', 'semantic'], function ($, Semantic) {
-        // $(document).on('click', '.chatting', function(e) {
-        //     $('#modal-' + '1-2').modal('show');
-        // });
-
         $(document).on('dblclick', '.profile .name', function(e) {
             var my_id = $(e.target).attr('id');
             $(location).attr('href', '' + '/chat/' + my_id);
         });
 
-        // $(document).on('click', '.buttons', function(e) {
-        //     $('.katalk.input').show();
-        //     $('#modal-1').modal('hide');
-        // });
-        // $(document).on('click', '.close.icon', function(e) {
-        //     $('.katalk.input').hide();
-            
-        // });
-
+        $(document).on('click', '.header .comment.icon', function(e) {
+            var my_id = $(e.target).attr('id');
+            $(location).attr('href', '' + '/chat/' + my_id);
+        });
     });
 });
